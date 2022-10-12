9b7c7f0bfef17e5473db67748e5ef85cd0209bc2
Ronald Li <Ronaldnyc88@gmail.com>
2022-10-12 17:19:39 -0400

    added delete comment, and username

--- models/Comment.js
+++ models/Comment.js
@@ -13,10 +13,14 @@ const CommentSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "Post",
   },
-//   user: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: "User",
-//   },
+  createdBy: {
+    type: String,
+    ref: "User"
+  },
+  createdById: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
   createdAt: {
     type: Date,
     default: Date.now,
---
+++ routes/comments.js
@@ -0,0 +1,16 @@
+const { Router } = require("express");
+const express = require("express");
+const router = express.Router();
+const commentsController = require("../controllers/comments");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+//Comment Routes - simplified for now
+
+
+router.post("/createComment/:id", commentsController.createComment);
+router.delete("/deleteComment/:postid/:commentid", commentsController.deleteComments);
+
+
+// router.delete("/deletePost/:id", postsController.deletePost);
+
+module.exports = router;
